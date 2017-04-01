9d90029a1ebd8c45bbb08d344b7450e0b7367921
trungcheng <trungs1bmt@gmail.com>
2017-04-01 18:41:25 +0700

    fix path

--- server/server.js
+++ server/server.js
@@ -27,18 +27,18 @@ app.use(bodyParser.json());
 
 app.use(expressValidator());
 
-app.use('/client', express.static('../client'));
-app.use('/node_modules', express.static('../node_modules'));
-app.use('/server', express.static('../server'));
+app.use('/client', express.static('./client'));
+app.use('/node_modules', express.static('./node_modules'));
+app.use('/server', express.static('./server'));
 
 app.use('/api', apiRoute);
 
 app.get('/', function(req, res) {
-	res.sendFile('index.html', {root: '../client/'});
+	res.sendFile('index.html', {root: './client/'});
 });
 
-server.listen(3002, function(){
-	console.log('App listening to http://localhost:3002');
+server.listen(1337, function() {
+	console.log('App listening to http://localhost:1337');
 });
 
 
