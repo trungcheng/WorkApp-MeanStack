eb1c99d56fa5ae7759b50f19a5c2d956b2a5f0d9
trungcheng <trungs1bmt@gmail.com>
2017-04-10 22:15:46 +0700

    fix v2

--- server/server.js
+++ server/server.js
@@ -27,20 +27,20 @@ app.use(bodyParser.json({limit:'50mb'}));
 
 app.use(expressValidator());
 
-app.use('/client', express.static('../client'));
-app.use('/node_modules', express.static('../node_modules'));
-app.use('/server', express.static('../server'));
+app.use('/client', express.static('./client'));
+app.use('/node_modules', express.static('./node_modules'));
+app.use('/server', express.static('./server'));
 
 app.use('/api', apiRoute);
 
 app.get('/', function(req, res) {
-	res.sendFile('index.html', {root: '../client/'});
+	res.sendFile('index.html', {root: './client/'});
 });
 
 var port = process.env.PORT || 3000;
 
 server.listen(port, function() {
-	console.log('App listening on' + port);
+	console.log("Express server listening on port %d in %s mode", this.address().port, server.settings.env);
 });
 
 
