911ea531eec9875ff7b7c83e7f36e37752e58136
trungcheng <trungs1bmt@gmail.com>
2017-04-10 22:56:22 +0700

    fix

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 module.exports = {
     'secret': 'eDW7Cs7VhSGb52INec9GbZeKAozL1Hyu',
     'expiresInMinutes':1440,
-    'database': 'process.env.MONGOLAB_URI || mongodb://trungcheng:trungcheng@ds157740.mlab.com:57740/mean-trello',
+    'database': 'mongodb://trungcheng:trungcheng@ds157740.mlab.com:57740/mean-trello',
     'socketClient':'http://localhost:3002',
     'environment':'develop',
 };
--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ io.on('connection', function(socket) {
     chatSocket.respond(socket, io.sockets);
 });
 
-mongoose.connect(config.database, function (err) {
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/meanpro1', function (err) {
 	if(err) throw err;
 	console.log('Connect to db success');
 });
@@ -37,7 +37,7 @@ app.get('/', function(req, res) {
 	res.sendFile('index.html', {root: './client/'});
 });
 
-server.listen(process.env.PORT || 3000, function(){
+server.listen(process.env.PORT || 5000, function(){
    var host = server.address().address;
    var port = server.address().port;
    console.log('Server listening on http://%s:%s', host, port);
