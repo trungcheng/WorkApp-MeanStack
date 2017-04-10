2c44a3a5bbacd3262401d24e9017a851c1f87e9b
trungcheng <trungs1bmt@gmail.com>
2017-04-10 22:36:37 +0700

    fix v3

--- server/server.js
+++ server/server.js
@@ -37,10 +37,8 @@ app.get('/', function(req, res) {
 	res.sendFile('index.html', {root: './client/'});
 });
 
-var port = process.env.PORT || 3000;
-
-server.listen(port, function() {
-	console.log("Express server listening on port %d in %s mode", this.address().port, server.settings.env);
+server.listen(process.env.PORT || 3000, function() {
+	console.log("Server running !");
 });
 
 
