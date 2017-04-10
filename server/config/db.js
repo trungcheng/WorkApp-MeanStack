f5cd6a8e3d982b6560ad254d1a4c5e2e6007eb8c
trungcheng <trungs1bmt@gmail.com>
2017-04-10 21:48:23 +0700

    add config db

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 module.exports = {
     'secret': 'eDW7Cs7VhSGb52INec9GbZeKAozL1Hyu',
     'expiresInMinutes':1440,
-    'database': 'mongodb://localhost:27017/meanpro1',
+    'database': 'mongodb://trungcheng:trungcheng@ds157740.mlab.com:57740/mean-trello',
     'socketClient':'http://localhost:3002',
     'environment':'develop',
 };
